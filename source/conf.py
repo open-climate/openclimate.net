974f4e922fc63dc0fc11a4da797af00fd6c8d551
isedwards <ian@myacorn.com>
2024-07-29 15:47:05 +0100

    Initial commit

---
+++ source/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sphinx-openclimate-theme
+
+project = 'Open Climate Community of Practice'
+copyright = '2024, Ian Edwards'
+author = 'Ian Edwards'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_openclimate_theme'
+html_theme_path = [sphinx_openclimate_theme.get_html_theme_path()]
+
+html_static_path = ['_static']
---
+++ source/index.rst
@@ -0,0 +1,17 @@
+.. Open Climate Community of Practice documentation master file, created by
+   sphinx-quickstart on Mon Jul 29 15:42:52 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Open Climate Community of Practice documentation
+================================================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
